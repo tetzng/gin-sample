a57a46cb93460a085a255a4817cf031e6688ee7f
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-19 01:04:34 +0900

    get posts

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	router := gin.Default()
+	router.GET("/posts", getPosts)
+
+	router.Run("localhost:8080")
+}
+
+func getPosts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, posts)
+}
+
+type post struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var posts = []post{
+	{ID: "1", Title: "Hello Gin", Body: "I started to learn golang."},
+	{ID: "2", Title: "Second Post", Body: "Second body."},
+	{ID: "3", Title: "TITLE", Body: "MY BODY"},
 }
